72987c15f4e7d4f26909197bf92a3860548309dc
Ryan Madsen <rm752@njit.edu>
2021-08-18 13:40:31 -0400

    Made the stack better

--- com/madsen/Main.java
+++ com/madsen/Main.java
@@ -6,16 +6,16 @@ public class Main
 {
     static long fibonacci(int n)
     {
-
-        Stack<Integer> stack = new Stack<>();
+        int[] stack = new int[256];
+        int stackPointer = 0;
         //0 is begining 1 is first call 2 is second call 3 is end
         int returnPointer = 0;
         long retReg = 0;
         int aReg = n;
         int bReg = 0;
 
-        //The other function pushing its return pointer
-        stack.push(3);
+        //The main function "pushing" its return pointer
+        stack[stackPointer++] = 3;
         while (true)
         {
             if (returnPointer == 0)
@@ -24,36 +24,35 @@ public class Main
                 {
                     retReg = 1;
                     //"return"
-                    returnPointer = stack.pop();
+                    returnPointer = stack[--stackPointer];
                     continue;
                 }
-                stack.push(bReg);
+                stack[stackPointer++] = bReg;
                 bReg = aReg;
                 aReg--;
 
                 //"call #1"
-                stack.push(1);
+                stack[stackPointer++] = 1;
                 returnPointer = 0;
             }
             else if (returnPointer == 1)
             {
-                stack.push((int) retReg);
-                stack.push((int) (retReg >> 32));
-
+                stack[stackPointer++] = (int) retReg;
+                stack[stackPointer++] = (int) (retReg >> 32);
                 aReg = bReg - 2;
                 //"call #2"
-                stack.push(2);
+                stack[stackPointer++] = 2;
                 returnPointer = 0;
             }
             else if (returnPointer == 2)
             {
 
-                retReg += (long) stack.pop() << 32;
-                retReg += stack.pop();
-                bReg = stack.pop();
+                retReg += (long) stack[--stackPointer] << 32;
+                retReg += stack[--stackPointer];
+                bReg = stack[--stackPointer];
 
                 //"return"
-                returnPointer = stack.pop();
+                returnPointer = stack[--stackPointer];
             }
             else if (returnPointer == 3)
             {
@@ -70,15 +69,16 @@ public class Main
     static long factorial(int n)
     {
 
-        Stack<Integer> stack = new Stack<>();
+        int[] stack = new int[100];
+        int stackPointer = 0;
         //0 is begining 1 is the call 2 is end
         int returnPointer = 0;
         long retReg = 0;
         int aReg = n;
         int bReg = 0;
 
-        //The other function pushing its return pointer
-        stack.push(2);
+        //The main function "pushing" its return pointer
+        stack[stackPointer++] = 2;
         while (true)
         {
             if (returnPointer == 0)
@@ -87,24 +87,24 @@ public class Main
                 {
                     retReg = 1;
                     //"return"
-                    returnPointer = stack.pop();
+                    returnPointer = stack[--stackPointer];
                     continue;
                 }
-                stack.push(bReg);
+                stack[stackPointer++] = bReg;
                 bReg = aReg;
                 aReg--;
 
                 //"call"
-                stack.push(1);
+                stack[stackPointer++] = 1;
                 returnPointer = 0;
             }
             else if (returnPointer == 1)
             {
                 retReg *= bReg;
-                bReg = stack.pop();
+                bReg = stack[--stackPointer];
 
                 //"return"
-                returnPointer = stack.pop();
+                returnPointer = stack[--stackPointer];
             }
             else if (returnPointer == 2)
             {
@@ -118,11 +118,11 @@ public class Main
     {
         for (int i = 0; i < 40; i++)
         {
-            System.out.println("fib(" + i + ")=" + fibonacci(i));
+            //System.out.println("fib(" + i + ")=" + fibonacci(i));
         }
         for (int i = 0; i < 20; i++)
         {
-            System.out.println(i + "!=" + factorial(i));
+            //System.out.println(i + "!=" + factorial(i));
         }
     }
 }
